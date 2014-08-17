6fa8d1ab6da934e5f9e7df8e7a022dfa218cfb0b
Vandenborne Stefan <stefan@illutek.be>
2014-08-17 11:04:05 +0200

    backup op 17082014

---
+++ templates/node.tpl.php
@@ -0,0 +1,24 @@
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>>
+      <?php print $title; ?>
+    </h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <?php if ($display_submitted): ?>
+    <div class="meta submitted">
+      <?php print $user_picture; ?>
+      <?php print $submitted; ?>
+    </div>
+  <?php endif; ?>
+
+  <div class="content clearfix"<?php print $content_attributes; ?>>
+    <?php
+      print render($content);
+    ?>
+  </div>
+
+</div>
